ea5af4a3b8e46da3d446f1be90131e50fa77e55c
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-21 02:49:04 -0400

    add `base-cwd` plugin to test support `App`

--- test/support/app.js
+++ test/support/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var Base = require('base');
+var cwd = require('base-cwd');
 var plugins = require('base-plugins');
 var option = require('base-option');
 var task = require('base-task');
 
 var App = module.exports = function App() {
   Base.call(this);
+  this.use(cwd());
   this.use(plugins());
   this.use(option());
   this.use(task());
